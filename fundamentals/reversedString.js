60d6b1f6e87e52e5f6bfab022888cd71a838c351
ogatalars <ogataogara@gmail.com>
2022-12-05 19:19:33 -0300

    reverse exercise

---
+++ fundamentals/reversedString.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// first solution found on 'sololearn'
+function solution(str) {
+  return str === "" ? "" : solution(str.substr(1)) + str.charAt(0);
+}
+//my solution
+
+// function solution(str) {
+//   return str.split("").reverse().join();
+// }
