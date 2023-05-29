2c18e53f642f23a463770c469c19ee1716c06819
ogatalars <ogataogara@gmail.com>
2023-05-29 19:08:49 -0300

    Fix: Numeros 1 a 10 exercise

---
+++ fundamentals/c#/numeros1a10.cs
@@ -0,0 +1,14 @@
+// Escreva um programa que exiba os números de 1 a 10 na tela.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
