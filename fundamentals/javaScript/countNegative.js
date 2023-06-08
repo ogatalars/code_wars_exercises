eb30429d8ae0a64b713e478e7afdf07062e6e68f
ogatalars <ogataogara@gmail.com>
2023-06-08 16:15:03 -0300

    Add: Count Negative

---
+++ fundamentals/javaScript/countNegative.js
@@ -0,0 +1,24 @@
+// Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+// Example 1:
+
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+
+
+const countNegatives = function(grid) {
+    let count = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) {
+                count++;
+            }
+        }
+    }
+    console.log(`There are ${count} numbers in the matrix`)
+    return count
+};
