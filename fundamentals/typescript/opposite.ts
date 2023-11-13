c264604c13ba69d4e99f4a8d0e402a0e5cf0cee8
ogatalars <ogataogara@gmail.com>
2023-11-13 20:42:36 -0300

    feat: new exercise

---
+++ fundamentals/typescript/opposite.ts
@@ -0,0 +1,7 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+class Kata {
+    static opposite(n: number) {
+      // your code here
+      return n * -1
+    }
+  }
